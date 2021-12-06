cc6092d196d95b00f005f180531f3ca205389b2f
lollofred <loridipalma@gmail.com>
2021-12-06 20:50:37 +0100

    WIP

--- app/src/androidTest/java/app/python/spamemail/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/python/spamemail/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.apppython", appContext.getPackageName());
+        assertEquals("app.python.spamemail", appContext.getPackageName());
     }
 }

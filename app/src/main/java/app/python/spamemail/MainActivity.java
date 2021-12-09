6ee355457ad3678fb7c66ae7ad41400ca2137bbb
lollofred <loridipalma@gmail.com>
2021-12-10 00:56:37 +0100

    Fixing/Adding NEW: - Popup with checkbox - Setting Area (To Finish) - Cache Usr/Passw/CleanFields

--- app/src/main/java/app/python/spamemail/MainActivity.java
+++ app/src/main/java/app/python/spamemail/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.LinkMovementMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +21,9 @@ import android.widget.TextView;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
+
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     EditText NMessage;
     Button Spam;
     TextView output;
+
+    SharedPreferences preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
         Spam = (Button)findViewById(R.id.run);
         output = (TextView)findViewById(R.id.output);
 
+        //Inizialize Preferenze
+        preference = getApplicationContext().getSharedPreferences("Preference",MODE_PRIVATE);
+
+        String UsrCache = preference.getString("Usr","");
+        String PasswCache = preference.getString("Passw","");
+
+        if(!UsrCache.isEmpty())
+            From.setText(UsrCache);
+        if(!PasswCache.isEmpty())
+           Passw.setText(PasswCache);
+
+
+        //Link
         TextView link = (TextView) findViewById(R.id.link);
         link.setMovementMethod(LinkMovementMethod.getInstance());
 
@@ -63,12 +85,12 @@ public class MainActivity extends AppCompatActivity {
 
                 if(FieldNotBlank())
                 {
-                    String email = From.getText().toString();
-                    String passw = Passw.getText().toString();
-                    String email2 = To.getText().toString();
-                    String oggetto = Subject.getText().toString();
-                    String contenuto = Message.getText().toString();
-                    Integer NUMBER_OF_EMAILS = Integer.parseInt(NMessage.getText().toString());
+                   String email = From.getText().toString();
+                   String passw = Passw.getText().toString();
+                   String email2 = To.getText().toString();
+                   String oggetto = Subject.getText().toString();
+                   String contenuto = Message.getText().toString();
+                   Integer NUMBER_OF_EMAILS = Integer.parseInt(NMessage.getText().toString());
 
                     //and call main method inside script...//pass data here
                     PyObject obj = pyobj.callAttr("main",
@@ -81,20 +103,16 @@ public class MainActivity extends AppCompatActivity {
                     if(!obj.toString().startsWith("Error"))
                     {
                         //Popup
-                        CharSequence[] charSequence = new CharSequence[] {"Remember chose"};
-    
+
                         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        View mView = getLayoutInflater().inflate(R.layout.popup, null);
+                        CheckBox mCheckBox = mView.findViewById(R.id.checkBox);
     
                         builder.setCancelable(true);
                         builder.setTitle("Clear Fields");
                         builder.setMessage("Do you want to empty fields?");
-    
-                        builder.setSingleChoiceItems(charSequence, 0, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //TODO
-                            }
-                        });
+
+                        builder.setView(mView);
     
                         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                             @Override
@@ -109,7 +127,31 @@ public class MainActivity extends AppCompatActivity {
                                 CleanField();
                             }
                         });
-                        builder.show();
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+
+                        //To Handle Save Setting and not ask again Popup
+                        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                                if(compoundButton.isChecked()){
+                                    storeDialogStatus(true);
+                                }else{
+                                    storeDialogStatus(false);
+                                }
+                            }
+                        });
+
+                        if(getDialogStatus()){
+                            dialog.hide();
+                        }else{
+                            dialog.show();
+                        }
+
+                        //Save Usr e passw in cache
+                        preference.edit().putString("Usr",email).apply();
+                        preference.edit().putString("Passw",passw).apply();
                     }
                 }
             }
@@ -117,6 +159,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     public void ShowHidePass(View view){
 
         if(view.getId()==R.id.show_pass_btn){
@@ -166,5 +230,13 @@ public class MainActivity extends AppCompatActivity {
         else
             res = true;
         return res;
+    }
+
+    private void storeDialogStatus(boolean isChecked){
+        preference.edit().putBoolean("popup",isChecked).apply();
+    }
+
+    private boolean getDialogStatus(){
+        return preference.getBoolean("popup", false);
     }
 }
---
+++ app/src/main/java/app/python/spamemail/SettingsActivity.java
@@ -0,0 +1,47 @@
+package app.python.spamemail;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import java.util.Map;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+
+        //To get Settings
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Map<String, ?> Setting = preferences.getAll();
+        //To set a new Setting
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("Test", false);
+        editor.commit();
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.settings, rootKey);
+        }
+    }
+}
